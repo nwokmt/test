bc87f2f21ca3556c1605b61bb20bde8b94c443ff
nwokmt <kurage@big.or.jp>
2019-03-02 16:54:26 +0900

    migration profiles items

---
+++ database/migrations/2019_03_02_074323_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+            Schema::create('profiles', function (Blueprint $table) {	
+            	$table->increments('id');
+            	$table->string('email')->nullable();
+            	$table->string('name');
+            	$table->text('introduction')->nullable();
+            	$table->text('image')->nullable();
+            	$table->timestamp('created_at')->nullable();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
