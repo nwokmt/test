a95638ca88b597559a229ac51c7c370c1341925c
nwokmt <kurage@big.or.jp>
2019-03-03 23:00:28 +0900

    test

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -39,8 +39,6 @@ class ProfileController extends Controller
 
     public function front(Request $request)
    {
-echo "OK";
-return;	
 	$id = Auth::id();
 	$profiles = Profile::find($request->user_id = $id);
 
