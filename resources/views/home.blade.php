3eeae9116d145cb91561ae4b3d2726ffffe8dc4e
nwokmt <kurage@big.or.jp>
2019-03-03 23:09:21 +0900

    menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <a href="/admin/profile/front">プロフィール</a>
                 </div>
             </div>
         </div>
