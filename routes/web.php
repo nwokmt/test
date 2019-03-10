eff8fbd2a4291ec027c7417a70af857b2a9fed69
nwokmt <kurage@big.or.jp>
2019-03-10 22:05:46 +0900

    route

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+//トップページ
 Route::get('/', 'TopController@index')->name('top');
+//商品詳細
+Route::get('/detail/{id}', 'OrderController@detail')->where(['id'=>'[0-9]+']);
+//カート
+Route::get('/cart', 'OrderController@cart')->name('cart');
+
+
+//管理画面
 Route::group(['prefix' => 'admin'], function() {
     Route::get('profile', 'Admin\ProfileController@front')->name('front')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@add')->name('profile')->middleware('auth');
@@ -21,7 +30,7 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('item/edit/{id}', ['as' => 'edit', 'uses' => 'Admin\ItemController@edit'])->where(['id'=>'[0-9]+'])->middleware('auth');
     Route::post('item/save', ['as' => 'save', 'uses' => 'Admin\ItemController@save'])->middleware('auth');
 });
-
+//管理者ログイン後ホーム
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
