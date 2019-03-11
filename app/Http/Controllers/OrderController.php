0a217617a3830a9bc77a915fa4d8419d81c99e76
nwokmt <kurage@big.or.jp>
2019-03-11 11:41:17 +0900

    order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,12 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Item;
-use App\User;
-use Illuminate\Support\facades\Auth;
 
 class OrderController extends Controller
 {
