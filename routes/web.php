5114c14ffa12f6255c6de68f857a5b2373340d6b
nwokmt <kurage@big.or.jp>
2019-03-11 12:05:29 +0900

    route

--- routes/web.php
+++ routes/web.php
@@ -21,17 +21,26 @@ Route::get('/add/{id}', 'OrderController@add')->where(['id'=>'[0-9]+']);
 Route::get('/remove/{id}', 'OrderController@remove')->where(['id'=>'[0-9]+']);
 //カート
 Route::get('/cart', 'OrderController@cart')->name('cart');
+//注文
+Route::get('/order', 'OrderController@order');
 
 
 //管理画面
 Route::group(['prefix' => 'admin'], function() {
+    //プロフィール詳細
     Route::get('profile', 'Admin\ProfileController@front')->name('front')->middleware('auth');
+    //プロフィール編集
     Route::get('profile/edit', 'Admin\ProfileController@add')->name('profile')->middleware('auth');
     Route::post('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    //商品一覧
     Route::get('item', 'Admin\ItemController@list')->name('item')->middleware('auth');
+    //商品詳細
     Route::get('item/detail/{id}', ['as' => 'detail', 'uses' => 'Admin\ItemController@detail'])->where(['id'=>'[0-9]+'])->middleware('auth');
+    //商品追加
     Route::get('item/add', ['as' => 'add', 'uses' => 'Admin\ItemController@add'])->middleware('auth');
+    //商品編集
     Route::get('item/edit/{id}', ['as' => 'edit', 'uses' => 'Admin\ItemController@edit'])->where(['id'=>'[0-9]+'])->middleware('auth');
+    //商品保存
     Route::post('item/save', ['as' => 'save', 'uses' => 'Admin\ItemController@save'])->middleware('auth');
 });
 //管理者ログイン後ホーム
