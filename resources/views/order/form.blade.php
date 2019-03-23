86f4f28130276239f47da532393b2940fa82c50e
nwokmt <kurage@big.or.jp>
2019-03-23 23:33:21 +0900

    order

--- resources/views/order/form.blade.php
+++ resources/views/order/form.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <h1>プロフィール編集</h1>
+                <h1>注文内容</h1>
                 <form action="{{ action('OrderController@confirm') }}" method="post" enctype="multipart/form-data">
 
                     @if (count($errors) > 0)
@@ -20,37 +20,73 @@
                       <div class="form-group row">
                           <label class="col-md-2" for="name">名前</label>
                               <div class="col-md-6">
-                              <input type="text" class="form-control" name="name" value="{{ old('name',$profiles->name) }}">
+                              <input type="text" class="form-control" name="name" value="{{ old('name',$orders->name) }}">
                           </div>
                       </div>
-                          <div class="form-group row">
-                              <label class="col-md-2" for="introduction">自己紹介欄</label>
-                              <div class="col-md-9">
-                                <textarea class="form-control" name="introduction" rows="8">{{ old('introduction',$profiles->introduction) }}</textarea>
-                              </div>
-                         </div>
-                         <div class="form-group row">
-                             <label class="col-md-2" for="image">画像</label>
-                             <div class="col-md-8">
-<!------ img ------>
-<label class="verify_person__formbox__item_profile-img-upload" id="photo_frame1">
-<input type="file" accept="image/*" class="fileData">
-<div id="description1">
-			@if (old('image') || !empty($profiles->image))
-                <img src="{{ old('image',$profiles->image) }}" id="image_thum" width="150">
-                <input type="hidden" name="image" id="image_src" value="{{ old('image',$profiles->image) }}">
-            @else
-                <img src="/img/noimg.png" id="image_thum" width="150">
-                <input type="hidden" name="image" id="image_src" value="{{ old('image') }}">
-			@endif
-</div>
-</label>
-<!------ img ------>
+                      <div class="form-group row">
+                          <label class="col-md-2" for="name">郵便番号</label>
+                              <div class="col-md-6">
+                              <input type="text" class="form-control" name="postalcode" value="{{ old('name',$orders->postalcode) }}">
+                          </div>
+                      </div>
+                      <div class="form-group row">
+                          <label class="col-md-2" for="name">住所</label>
+                              <div class="col-md-6">
+                              <input type="text" class="form-control" name="address" value="{{ old('name',$orders->address) }}">
+                          </div>
+                      </div>
+                      <div class="form-group row">
+                          <label class="col-md-2" for="name">支払い方法</label>
+                              <div class="col-md-6">
+                              <input type="text" class="form-control" name="address" value="{{ old('name',$orders->address) }}">
+                          </div>
+                      </div>
+
+<!-- カート--->
+<hr>
+
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th colspan="5">注文商品</th>
+                            </tr>
+                            <tr>
+                                <th width="20%">画像</th>
+                                <th width="20%">商品名</th>
+                                <th width="10%">金額</th>
+                                <th width="40%">説明</th>
+                                <th width="10%"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                             @foreach($items as $key=>$item)
+                                <tr>
+<td>
+@if(empty($item->image))
+                                <img src="/img/noimg.png" id="image_thum" width="150">
+@else
+                                  <img src="{{ $item->image }}" id="image_thum" width="150">
+@endif
+</td>
+                                    <td>{{ ($item->name) }}</td>
+                                    <td>{{ ($item->price) }}円</td>
+                                    <td>{{ ($item->description) }}</td>
+				    <td align="center">
+ 					<a class="btn btn-primary" href="{{ action('OrderController@detail', ['id' => $item->id]) }}">詳細</a><br><br>
+ 					<a class="btn btn-primary" href="{{ action('OrderController@remove', ['id' => $key]) }}">削除</a>
+                                        
+                                    </td>
+                                </tr>
+                             @endforeach
+                        </tbody>
+                    </table>
+<hr>
+
                              </div>
                          </div>
                          {{ csrf_field() }}
                          <div class="row justify-content-center">
-                         <input type="submit" class="btn btn-warning" value="更新">
+                         <input type="submit" class="btn btn-warning" value="注文内容確認">
                          </div>
                     </div>
                     </div>
