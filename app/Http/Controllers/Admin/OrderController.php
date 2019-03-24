1ca1ccc999a757beff3610021121bae13b4b5299
nwokmt <kurage@big.or.jp>
2019-03-24 18:42:27 +0900

    admin order

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Item;
 use App\User;
 use App\Order;
+use App\Orderdetail;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\facades\Auth;
 
@@ -14,24 +15,15 @@ class OrderController extends Controller
 {
     public function list()
    {
-/*
-        $orders = DB::table('orders')
-                    ->join('orderdetails','orders.id','=','orderdetails.order_id')
-                    ->join('items','items.id','=','orderdetails.item_id')
-                    ->select('orders.*', 'items.*')
-                    ->orderBy('orders.updated_at',"desc")
-                    ->get();
-*/
         $orders = Order::orderBy('orders.updated_at',"desc")->get();
-print_r($orders);
-print_r($orders->details);
-exit;
         return view('admin.order.list', ['orders' => $orders]);
      }
 
     public function detail($id)
    {
-	$item = Item::find($id);
+	$orderdetail = Orderdetail:::where('id',1)->orderBy('orders.id',"asc")->get();
+print_r($orderdetail);
+exit;
 	return view('admin.item.detail', ['item' => $item]);
 
      }
